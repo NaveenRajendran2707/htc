885ba8f1b2de83a896f196682dccd5dfd20ad7ba
keerthanaIAS <unstoppablegirl010203@gmail.com>
2024-12-08 20:26:58 +0530

    dynamic api page local to htc

--- frontend/src/api/dynamicAPI.js
+++ frontend/src/api/dynamicAPI.js
@@ -6,29 +6,29 @@ const dynamicAPI = async (method, url, obj = {}) => {
     switch (method) {
       case 'get':
         return await axios
-          // .get(`https://htcapi.aiotechsoft.com${url}`, config())
-          .get(`http://localhost:5000${url}`, config())
+          .get(`https://htcapi.aiotechsoft.com${url}`, config())
+          // .get(`http://localhost:5000${url}`, config())
           //.get(`http://3.27.248.191:5000${url}`, config())
           .then((res) => res.data)
 
       case 'post':
         return await axios
-          // .post(`https://htcapi.aiotechsoft.com${url}`, obj, config())
-          .post(`http://localhost:5000${url}`, obj, config())
+          .post(`https://htcapi.aiotechsoft.com${url}`, obj, config())
+          // .post(`http://localhost:5000${url}`, obj, config())
           //.post(`http://3.27.248.191:5000${url}`, obj, config())
           .then((res) => res.data)
 
       case 'put':
         return await axios
-          // .put(`https://htcapi.aiotechsoft.com${url}`, obj, config())
-          .put(`http://localhost:5000${url}`, obj, config())
+          .put(`https://htcapi.aiotechsoft.com${url}`, obj, config())
+          // .put(`http://localhost:5000${url}`, obj, config())
           //.put(`http://3.27.248.191:5000${url}`, obj, config())
           .then((res) => res.data)
 
       case 'delete':
         return await axios
-          // .delete(`https://htcapi.aiotechsoft.com${url}`, config())
-          .delete(`http://localhost:5000${url}`, config())
+          .delete(`https://htcapi.aiotechsoft.com${url}`, config())
+          // .delete(`http://localhost:5000${url}`, config())
           //.delete(`http://3.27.248.191:5000${url}`, config())
           .then((res) => res.data)
     }
